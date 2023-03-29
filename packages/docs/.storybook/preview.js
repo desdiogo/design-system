083aa93d09d637f9a996040398b5f0bd818a8802
Diogo Eleutero <desdiogo10@yahoo.com>
2023-03-29 07:29:25 -0300

    first commit

---
+++ packages/docs/.storybook/preview.js
@@ -0,0 +1,14 @@
+import { themes } from '@storybook/theming';
+
+export default {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  docs: {
+    theme: themes.dark,
+  }
+}
